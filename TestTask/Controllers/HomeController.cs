d18fd1a78a46e12c7dd6632c5ab60db0a58b04d7
KOrenits <karlisorenits@gmail.com>
2023-11-10 01:43:29 +0200

    added validation

--- TestTask/Controllers/HomeController.cs
+++ TestTask/Controllers/HomeController.cs
@@ -15,11 +15,25 @@ namespace TestTask.Controllers
         }
 
         [HttpPost("createTask")]
-        public IActionResult CreateTask([FromQuery] string title, [FromQuery] string description)
+        public IActionResult CreateTask([FromQuery] string? title, [FromQuery] string? description)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
+            }
+
+            if(title == null)
+            {
+                return BadRequest("Title is required");
+            }
+            if (description == null)
+            {
+                return BadRequest("Description is required");
+            }
+
+            if (_testTaskDbContext.Tasks.Any(t => t.Title == title))
+            {
+                return BadRequest("A task with the same title already exists");
             }
 
             // Create a new task with the input data
@@ -29,14 +43,18 @@ namespace TestTask.Controllers
                 Description = description
             };
             
-
             // Add the new task to the database context
             _testTaskDbContext.Tasks.Add(newTask);
 
             // Save changes to the database
             _testTaskDbContext.SaveChanges();
 
-            return Ok(_testTaskDbContext.Tasks);
+            var response = new
+            {
+                Message = "Added task",
+                Data = newTask
+            };
+            return Ok(response);
         }
 
         [HttpGet("getAllTasks")]
@@ -47,7 +65,12 @@ namespace TestTask.Controllers
                 return BadRequest("There are no tasks created");
             }
 
-            return Ok(_testTaskDbContext.Tasks);
+            var response = new
+            {
+                Message = "List of all Tasks",
+                Data = _testTaskDbContext.Tasks
+            };
+            return Ok(response);
         }
 
         [HttpGet("getTaskById")]
@@ -58,32 +81,60 @@ namespace TestTask.Controllers
                 return BadRequest("There are no tasks created");
             }
 
-            var existingTask = _testTaskDbContext.Tasks.FirstOrDefault(t => t.Id == id);
+            var foundedTask = _testTaskDbContext.Tasks.FirstOrDefault(t => t.Id == id);
 
-            return Ok(existingTask);
+            var response = new
+            {
+                Message = "Founded Task",
+                Data = foundedTask
+            };
+            return Ok(response);
         }
 
         [HttpPut("updateTaskById")]
-        public IActionResult UpdateTask([FromQuery] int id, [FromQuery] string title, [FromQuery] string description)
+        public IActionResult UpdateTask([FromQuery] int id, [FromQuery] string? title, [FromQuery] string? description)
         {
             if (!_testTaskDbContext.Tasks.Any())
             {
                 return BadRequest("There are no tasks created");
             }
 
-            var existingTask = _testTaskDbContext.Tasks.FirstOrDefault(t => t.Id == id);
+            var previousTask = _testTaskDbContext.Tasks.FirstOrDefault(t => t.Id == id);
 
-            if (existingTask == null)
+            if (previousTask== null)
             {
                 return NotFound("Task not found with the specified ID.");
             }
 
-            existingTask.Title = title;
-            existingTask.Description = description;
+            if (title == null)
+            {
+                return BadRequest("Title is required");
+            }
+            if (description == null)
+            {
+                return BadRequest("Description is required");
+            }
+
+
+            if (_testTaskDbContext.Tasks.Any(t => t.Title == title))
+            {
+                return BadRequest("A task with the same title already exists");
+            }
+
+            var updatedTask = previousTask;
+            updatedTask.Title = title;
+            updatedTask.Description = description;
 
             _testTaskDbContext.SaveChanges();
 
-            return Ok(existingTask);
+            var response = new
+            {   
+                PreviousMessage = "Task before Update",
+                PreviousData = previousTask,
+                Message = "Updated Tas Datk",
+                Data = updatedTask
+            };
+            return Ok(response);
         }
 
         [HttpDelete("deleteTaskById")]
@@ -102,8 +153,14 @@ namespace TestTask.Controllers
             }
 
             _testTaskDbContext.Tasks.Remove(deletedTask);
+            _testTaskDbContext.SaveChanges();
 
-            return Ok("task with Id " + deletedTask.Id + "has been removed");
+            var response = new
+            {
+                Message = "This task has been deleted",
+                Data = deletedTask
+            };
+            return Ok(response);
         }
     }
 }
